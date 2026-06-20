310353d3cacc07a2aed5890558b41c6dc3922b82
KAMRAN <kamranrezaeii.01@gmail.com>
2026-06-20 22:36:31 +0330

    create custom Model USER

--- app/accounts/models.py
+++ app/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        EMPLOYER = "EMPLOYER", "Employer"
+        JOB_SEEKER = "JOB_SEEKER", "Job Seeker"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.JOB_SEEKER,
+    )
+
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
--- app/config/settings.py
+++ app/config/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "accounts",
+    "companies",
+    "jobs",
+    "applications",
 ]
 
 MIDDLEWARE = [
@@ -84,6 +89,11 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
@@ -102,6 +112,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = "accounts.User"
 
 
 # Internationalization
